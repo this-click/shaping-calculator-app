cdd499688fc4781c2fe87f49b454fd87d59373b0
this-click <88038974+this-click@users.noreply.github.com>
2023-01-30 15:48:41 +0100

    call view model when clicking button - initial commit

--- app/src/main/java/com/example/shapingcalculator/MainActivity.kt
+++ app/src/main/java/com/example/shapingcalculator/MainActivity.kt
@@ -2,10 +2,42 @@ package com.example.shapingcalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import com.example.shapingcalculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    private val viewModel: KnittingViewModel by viewModels {
+        KnittingViewModelFactory(
+            (application as KnittingApplication).database
+                .shapedItemDao()
+        )
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.calculateButton.setOnClickListener {
+            if (isEntryValid()) {
+                viewModel.addNewItem(
+                    binding.gaugeEditText.toString(),
+                    binding.lengthEditText.text.toString(),
+                    binding.incsPerRowEditText.text.toString(),
+                    binding.incsEditText.text.toString()
+                )
+            }
+        }
+    }
+
+    private fun isEntryValid(): Boolean {
+        return viewModel.isEntryValid(
+            binding.gaugeEditText.toString(),
+            binding.lengthEditText.text.toString(),
+            binding.incsPerRowEditText.text.toString(),
+            binding.incsEditText.text.toString()
+        )
     }
 }
