65bfd111534e42b96837fd5835fb96e91ed7e84e
this-click <88038974+this-click@users.noreply.github.com>
2023-02-01 17:17:59 +0100

    display values read from DB - partial

--- app/src/main/java/com/example/shapingcalculator/CalculateShapingFragment.kt
+++ app/src/main/java/com/example/shapingcalculator/CalculateShapingFragment.kt
@@ -10,6 +10,8 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.shapingcalculator.data.ShapedItem
 import com.example.shapingcalculator.databinding.FragmentCalculateShapingBinding
+import java.math.RoundingMode
+import kotlin.math.floor
 
 
 class CalculateShapingFragment : Fragment() {
@@ -22,6 +24,7 @@ class CalculateShapingFragment : Fragment() {
                 .shapedItemDao()
         )
     }
+    private var shapingValues: MutableList<String> = mutableListOf()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -38,7 +41,22 @@ class CalculateShapingFragment : Fragment() {
         viewModel.getItem().observe(this.viewLifecycleOwner) { selectedItem ->
             if (selectedItem != null) {
                 item = selectedItem
-                binding.result1TextView.text = item.increasesRow
+                //FIXME: Cards with new values after entering new values and tapping Calculate
+                shapingValues = calculateShaping(
+                    item.rowGauge.toDouble(),
+                    item.shapingLength.toDouble(),
+                    item.increasesTotal.toInt(),
+                    item.increasesRow.toInt()
+                )
+                // TODO: display placeholder while data is loaded from DB. Sometimes I can see $s
+                binding.result1TextView.text = resources.getString(
+                    R.string.shaping_rate,
+                    shapingValues[2], shapingValues[1]
+                )
+                binding.result2TextView.text = resources.getString(
+                    R.string.shaping_rate,
+                    shapingValues[3], shapingValues[0]
+                )
             }
         }
 
@@ -52,6 +70,7 @@ class CalculateShapingFragment : Fragment() {
                 )
 
                 getView()?.rootView?.windowToken
+                // Close the keyboard after clicking Calculate, if data entered is valid
                 closeSoftKeyboard()
             }
         }
@@ -76,5 +95,48 @@ class CalculateShapingFragment : Fragment() {
             view = View(activity)
         }
         imm.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+
+    private fun calculateShaping(
+        rowGauge: Double,
+        shapingLength: Double,
+        totalIncs: Int,
+        incsPerRow: Int
+    ): MutableList<String> {
+        /* See README for formula explanation */
+
+        val overRows = shapingLength * rowGauge
+        var nbIncsRow = incsPerRow
+        if (incsPerRow == 0) {
+            nbIncsRow += 1
+        }
+        // TODO: Formula needs more work
+        var shapingCalculated = totalIncs / nbIncsRow
+        if (shapingCalculated == 0) {
+            shapingCalculated += 1
+        }
+        val basicShapingRate = roundDownToTwoDecimals(overRows / shapingCalculated)
+        val shapingRateItrA = floor(basicShapingRate)
+        val shapingRateItrB = shapingRateItrA + 1
+        val remainder = roundDownToTwoDecimals(overRows % shapingCalculated)
+        val rateFactorA = roundDownToTwoDecimals(shapingCalculated - remainder)
+        val rateFactorB = remainder
+
+        shapingValues.addAll(
+            arrayListOf(
+                rateFactorA.toString(),
+                rateFactorB.toString(),
+                shapingRateItrA.toString(),
+                shapingRateItrB.toString()
+            )
+        )
+
+        return shapingValues
+    }
+
+    private fun roundDownToTwoDecimals(amount: Double): Double {
+        // Need BigDec to format with 2 decimals
+        val bigDecimal = amount.toBigDecimal().setScale(2, RoundingMode.FLOOR)
+        return bigDecimal.toDouble()
     }
 }
